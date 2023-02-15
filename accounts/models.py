32662dc7421a25aae07aee919cdd2bc2d4b01305
Somaye Jalili <s68jalili@gmail.com>
2023-02-15 03:50:39 -0800

    add article models

--- accounts/models.py
+++ accounts/models.py
@@ -32,3 +32,18 @@ class Admin(CustomUser):
         proxy = True
         verbose_name = 'مدیر'
         verbose_name_plural = 'مدیران'
+
+
+class Profile(models.Model):
+    pass
+
+
+class Article(models.Model):
+    RELEASE = [('Publish', 'Publish'), ('Draft', 'Draft')]
+    title = models.CharField(max_length=50)
+    text = models.CharField(max_length=500)
+    release_mode = models.CharField(max_length=1, choices=RELEASE, default='Draft')
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    release_date = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='article_image/')
