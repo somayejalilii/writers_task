431bb5d9b8ea281973c293dcbee47094276bac44
Somaye Jalili <s68jalili@gmail.com>
2023-02-22 10:44:34 -0800

    add view and update models

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+
 
 # Create your models here.
 
@@ -34,10 +36,6 @@ class Admin(CustomUser):
         verbose_name_plural = 'مدیران'
 
 
-class Profile(models.Model):
-    pass
-
-
 class Article(models.Model):
     RELEASE = [('Publish', 'Publish'), ('Draft', 'Draft')]
     title = models.CharField(max_length=50)
@@ -47,3 +45,22 @@ class Article(models.Model):
     update_date = models.DateTimeField(auto_now=True)
     release_date = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='article_image/')
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    phone = models.IntegerField(null=True, blank=True)
+    address = models.CharField(max_length=500, null=True, blank=True)
+    profile_image = models.ImageField(upload_to='profile/', null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+def save_profile_user(sender, **kwargs):
+    if kwargs['created']:
+        profile_user = Profile(user=kwargs['instance'])
+        profile_user.save()
+
+
+post_save.connect(save_profile_user, sender=CustomUser)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from .models import Profile
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+# Create your views here
+
+@csrf_exempt
+def create_profile(request):
+
+    try:
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        password = request.POST.get('password')
+        phone = request.POST.get('phone')
+        address = request.POST.get('address')
+        profile = Profile()
+
+        profile.first_name = first_name
+        profile.last_name = last_name
+        profile.password = password
+        profile.phone = phone
+        profile.address = address
+        profile.save()
+
+        return HttpResponse("200 OK")
+
+    except:
+        return HttpResponse("NOT OK")
+
+@csrf_exempt
+def update_profile(request):
+    try:
+        profile = Profile
+        id = request.POST.get('id')
+        password = request.Post.get('password')
+        phone = request.Post.get('phone')
+        address = request.Post.get('address')
+
+        profile.objects.filter(id=id).update(phone=phone, password=password, address=address)
+        return HttpResponse("200 OK")
+
+    except:
+        return HttpResponse("Not Ok")
+
+
+
+
+
+
